8236a7c67b133a3ae30a5547ef4d7ea8815cc101
julimanesi94 <julimanesi94@gmail.com>
2020-11-22 20:00:54 -0300

    Agregadas validaciones en perfil y Horas trabajadas

--- TpIngSoftII/Services/HorasTrabajadasService.cs
+++ TpIngSoftII/Services/HorasTrabajadasService.cs
@@ -21,6 +21,13 @@ namespace TpIngSoftII.Services
         }
 
         /* Hacer Override de los metodos que necesite customizar (validaciones, logicas, etc.) heredados de EntityAppServiceBase */
-
+        protected override void ValidarEntityUpdating(HorasTrabajadas entity, HorasTrabajadasDto dto, bool isNew)
+        {
+            if (dto.CantHoras<0) throw new System.ArgumentException("La cantidad de horas no puede ser negativo");
+            if (dto.Fecha == null || dto.Fecha == DateTime.MinValue) throw new System.ArgumentException("La Fecha es obligatoria");
+            if (string.IsNullOrWhiteSpace(dto.ProyectoID.ToString())) throw new System.ArgumentException("El id de proyecto es obligatorio");
+            if (string.IsNullOrWhiteSpace(dto.TareaID.ToString())) throw new System.ArgumentException("La id de tarea es obligatoria");
+            if (string.IsNullOrWhiteSpace(dto.HorasTrabajadasEstadoID.ToString())) throw new System.ArgumentException("La id del estado de horas trabajadas es obligatoria");
+        }
     }
 }
