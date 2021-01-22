ba2118308026827efc9b7e1ee1da85a286fcc539
julimanesi94 <julimanesi94@gmail.com>
2021-01-22 20:06:58 -0300

    validaciones Tarea por Empleado.-

--- TpIngSoftII/Models/DTOs/TareaDto.cs
+++ TpIngSoftII/Models/DTOs/TareaDto.cs
@@ -9,9 +9,14 @@ namespace TpIngSoftII.Models.DTOs
     public class TareaDto : IDto
     {
         public int ID { get; set; }
+
         public int ProyectoID { get; set; }
-        public int EmpleadoID { get; set; }
-        public int PerfilID { get; set; }
+        public string ProyectoNombre { get; set; }
+
+        public int EmpleadoPerfilID { get; set; }
+        public string EmpleadoPerfilNombreEmplado { get; set; }
+        public string EmpleadoPerfilDescripcionPerfil { get; set; }
+
         public string Nombre { get; set; }
         public decimal HorasEstimadas { get; set; }
         public decimal HorasOB { get; set; }
--- TpIngSoftII/Services/TareaService.cs
+++ TpIngSoftII/Services/TareaService.cs
@@ -15,12 +15,22 @@ namespace TpIngSoftII.Services
 {
     public class TareaService : EntityAppServiceBase<Tarea, TareaDto>, ITareaService
     {
+        private readonly IEntityBaseRepository<Empleado> empleadoRepository;
 
-        public TareaService(IEntityBaseRepository<Tarea> entityRepository, IUnitOfWork unitOfWork, IAppContext appContext) : base(entityRepository, unitOfWork, appContext)
+        public TareaService(IEntityBaseRepository<Tarea> entityRepository, IUnitOfWork unitOfWork, IAppContext appContext, IEntityBaseRepository<Empleado> empleadoRepository) : base(entityRepository, unitOfWork, appContext)
         {
+            this.empleadoRepository = empleadoRepository;
         }
 
-        /* Hacer Override de los metodos que necesite customizar (validaciones, logicas, etc.) heredados de EntityAppServiceBase */
+        protected override void ValidarEntityUpdating(Tarea entity, TareaDto dto, bool isNew)
+        {
+            //obtengo el proyecto de esta tarea en particular para ese empleado //CORREGIR ESTO
+            //var proyecto = proyectoRepository.AllIncludingAsNoTracking(x => x.Tareas).Select(y => y.ID == dto.ProyectoID);
+            var tarea = entityRepository.AllIncludingAsNoTracking(x => x.EmpleadoPerfil)
+                                        .FirstOrDefault(x => x.ProyectoID == dto.ProyectoID 
+                                                             && x.EmpleadoPerfil.EmpleadoID == /* EmpleadoID */);
+            // Buscar EmpleadoID en Perfiles y comparar para traer tarea en base a ese EmpleadoID
 
+        }
     }
 }
