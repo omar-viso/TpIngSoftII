a3b4cc08c2e4e8448c23efb9ff4868b42cf2f0b0
Omar Viso <o.viso@hotmail.com>
2021-02-20 19:56:34 -0300

    Se agrega endpoint HorasTrabajadasPorProyectoPorPerfilPorEmpleadoTotales para Horas por Proyecto Perfil Empleados.-

---
+++ TpIngSoftII/Models/DTOs/ProyectoPerfilesEmpleadosHorasDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TpIngSoftII.Interfaces;
+
+namespace TpIngSoftII.Models.DTOs
+{
+    public class ProyectoPerfilesEmpleadosHorasDto
+    {
+        public string ProyectoNombre { get; set; }
+        public ICollection<PerfilEmpleadosHorasDto> PerfilesEmpleadosHoras {get;set;}
+    }
+}
